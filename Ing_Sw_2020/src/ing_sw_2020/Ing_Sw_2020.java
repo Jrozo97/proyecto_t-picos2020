dffc88ccf8c90cbf7b25e583495a8a23b671aff5
Jrozo <Jrozo@DESKTOP-031E2FB>
2020-05-05 14:29:37 -0500

    Prueba

---
+++ Ing_Sw_2020/src/ing_sw_2020/Ing_Sw_2020.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ing_sw_2020;
+
+/**
+ *
+ * @author Jrozo
+ */
+public class Ing_Sw_2020 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Inicio del proyecto mayo 4 11:40");
+        
+        
+    }
+    
+}
